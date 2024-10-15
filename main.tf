f4c7591131f43bbb5c4d9ab9e0995f5715f85ae3
tsingh <tsingh@atpco.net>
2024-10-15 14:06:27 -0700

    Changed blob store name to only contain lower case letters and numbers

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-    name = "${var.storage_account_name}_${var.environment}"
+    name = "${var.storage_account_name}${var.environment}"
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     account_tier = var.account_tier
