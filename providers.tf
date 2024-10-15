50168f46c0634e0f1de3b2b25bf2a4c127266bac
tsingh <tsingh@atpco.net>
2024-10-15 13:53:40 -0700

    removed the backup config as it needs to be statically defined

--- providers.tf
+++ providers.tf
@@ -5,12 +5,3 @@ provider "azurerm" {
     client_id       = var.azure_client_id
     tenant_id       = var.azure_tenant_id
 }
-
-terraform {
-    backend "azurerm" {
-        storage_account_name = azurerm_storage_account.storage_account.name
-        container_name       = azurerm_storage_container.terraform_state_container.name
-        key                  = "blobstorage.tfstate"
-        resource_group_name  = azurerm_resource_group.rg.name
-    }
-}
