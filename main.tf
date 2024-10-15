4fd2fcc83f9246ce2b23848df6784907b0a0b510
tsingh <tsingh@atpco.net>
2024-10-15 13:32:10 -0700

    Created a draft of terraform code to be deployed

---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_resource_group" "rg" {
+    name = var.rg_name
+    location = var.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+    name = var.storage_account_name
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    account_tier = var.account_tier
+    account_replication_type = var.account_replication_type
+
+    tags = {
+        environment = var.environment
+    }
+
+    network_rules {
+        default_action = "Deny"
+        ip_rules       = [var.allowed_ip]
+        bypass         = ["AzureServices"]
+    }
+}
+
+# Create the Blob Container
+resource "azurerm_storage_container" "terraform_state_container" {
+    name = var.container_name
+    storage_account_name = azurerm_storage_account.storage_account.name
+    container_access_type = var.access_type
+}
+
+# Define the Storage Management Policy (Lifecycle Policy)
+resource "azure_storage_management_policy" "Lifecycle_policy" {
+    storage_account_id = azurerm_storage_account.storage_account.id
+
+    rule {
+        name    = "delete-contents-after-1-day"
+        enabled = true
+
+        filters {
+            blob_types = ["blockBlob"]
+        }
+
+        actions {
+            delete {
+                days_after_modification_greater_than = 1 #Delete blobs after 1 day
+            }
+        }
+    }
+}
